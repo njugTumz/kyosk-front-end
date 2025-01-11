613940c999f57b19925aaab85994cab22bb67a6d
DESKTOP-ONN0I1E\HP <geoftums@gmail.com>
2025-01-11 19:10:31 +0300

    Moved books data to its own file

---
+++ src/data/booksData.ts
@@ -0,0 +1,19 @@
+import { Book } from '@/interfaces/book';
+
+export const books: Book[] = [
+  { title: '1984', author: 'George Orwell', year: 1949 },
+  { title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+  { title: 'Moby-Dick', author: 'Herman Melville', year: 1851 },
+  { title: 'War and Peace', author: 'Leo Tolstoy', year: 1869 },
+  { title: 'Pride and Prejudice', author: 'Jane Austen', year: 1813 },
+  { title: 'The Catcher in the Rye', author: 'J.D. Salinger', year: 1951 },
+  { title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+  { title: 'The Hobbit', author: 'J.R.R. Tolkien', year: 1937 },
+  { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', year: 1866 },
+  { title: 'The Odyssey', author: 'Homer', year: -700 },
+  { title: 'The Brothers Karamazov', author: 'Fyodor Dostoevsky', year: 1880 },
+  { title: 'Don Quixote', author: 'Miguel de Cervantes', year: 1605 },
+  { title: 'Les Misérables', author: 'Victor Hugo', year: 1862 },
+  { title: 'The Picture of Dorian Gray', author: 'Oscar Wilde', year: 1890 }
+];
